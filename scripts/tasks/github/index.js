b0ff57968a2ff1534fbe3a20be55c4c8182da6b1
philipplgh <philipp@ethereum.org>
2018-10-10 22:33:35 +0200

    cleanup

--- scripts/tasks/github/index.js
+++ scripts/tasks/github/index.js
@@ -4,20 +4,6 @@ const createRelease = require('./createRelease')
 const uploadAsset = require('./uploadAsset')
 const publishRelease = require('./publishRelease')
 
-const createChecksums = async () => {
-  return true
-}
-
-/*
-    console.log('uploading metadata')
-    await uploadAsset(metadataPath, 'metadata.json', draftRelease)
-
-    await uploadAsset(asarPath, 'react_ui.asar', draftRelease)
-    // TODO delete draft if uploads fail
-    console.log('step 6: publishing release')
-    await publishRelease(draftRelease)
-*/
-
 let githubTasks = [
    ['Get GitHub credentials', parseOptions ]
   ,['Connect to GitHub API', createAPI ]
@@ -27,6 +13,7 @@ let githubTasks = [
   ,['Upload app', async ({ github, appPath, draftRelease, githubOtions }) => 
   await uploadAsset(github, appPath, draftRelease, githubOtions) ]
   ,['Publish release', publishRelease ]
+  // TODO delete draft if uploads fail
 ]
 
 module.exports = githubTasks
