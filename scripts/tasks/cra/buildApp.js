7742ec0cf482cfbcbd8c210e37f485ee4d36de23
philipplgh <philipplgh@gmail.com>
2018-12-13 13:36:32 +0100

    add option to customize webpack config

--- scripts/tasks/cra/buildApp.js
+++ scripts/tasks/cra/buildApp.js
@@ -1,16 +1,27 @@
 const path = require('path')
 // use create-react-apps's webpack or should it have own dependency?
 const webpack = require('webpack')
+const fs = require('fs')
 
 process.env.NODE_ENV = "production"
 
-const config = require(path.join(process.env.NODE_MODULES_PATH, 'react-scripts', 'config', 'webpack.config.prod'))
+let config = require(path.join(process.env.NODE_MODULES_PATH, 'react-scripts', 'config', 'webpack.config.prod'))
 
 function build(previousFileSizes) {
   /*
   console.log('Creating an optimized production build...');
   require("react-scripts/scripts/build")
   */
+  const basePath = process.cwd()
+  let configCustomizerPath = path.join(basePath, 'webpack.config.js')
+  if(fs.existsSync(configCustomizerPath)){
+    try {
+      let configCustomizer = require(configCustomizerPath)
+      config = configCustomizer(config)
+    } catch (error) {
+      console.log('faled to modify config', error)
+    }
+  }
 
   let compiler = webpack(config);
   return new Promise((resolve, reject) => {
